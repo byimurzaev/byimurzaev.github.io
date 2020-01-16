26d2bd4d52b78d4feaed731f36490f62fdef4bfa
Timur <39311753+byimurzaev@users.noreply.github.com>
2020-01-17 00:00:24 +0300

    Гости v1
    Начал работу над полум гости

--- js/script.js
+++ js/script.js
@@ -1,8 +1,67 @@
-let a = 0;
+let guests = {
+    adults: 0,
+    children: 0,
+    babies: 0
+};
+let adultsValue = document.querySelector("#adultsValue");
+let adultsPlus = document.querySelector("#adultsPlus");
+let adultsMinus = document.querySelector("#adultsMinus");
+
+adultsPlus.addEventListener( 'click', () => {
+    guests.adults += 1;  
+    adultsValue.textContent = guests.adults;
+});
+
+adultsMinus.addEventListener( 'click', () => {
+    guests.adults -= 1;  
+    adultsValue.textContent = guests.adults;
+});
+
+let childrenValue = document.querySelector("#childrenValue");
+let childrenPlus = document.querySelector("#childrenPlus");
+let childrenMinus = document.querySelector("#childrenMinus");
+
+childrenPlus.addEventListener( 'click', () => {
+    guests.children += 1;  
+    childrenValue.textContent = guests.children;
+});
+
+childrenMinus.addEventListener( 'click', () => {
+    guests.children -= 1;  
+    childrenValue.textContent = guests.children;
+});
 
 
-document.getElementById("test").onclick = function(a){
-    a+=1;
-    console.log(a)
-}
+let babiesValue = document.querySelector("#babiesValue");
+let babiesPlus = document.querySelector("#babiesPlus");
+let babiesMinus = document.querySelector("#babiesMinus");
 
+babiesPlus.addEventListener( 'click', () => {
+    guests.babies += 1;  
+    babiesValue.textContent = guests.babies;
+});
+
+babiesMinus.addEventListener( 'click', () => {
+    guests.babies -= 1;  
+    babiesValue.textContent = guests.babies;
+});
+
+
+let guestsCancel = document.querySelector("#guestsCancel");
+let guestsApply = document.querySelector("#guestsApply");
+let questsInput = document.querySelector("#questsInput");
+let questsBlock = document.querySelector(".guests");
+
+
+guestsApply.addEventListener( 'click', () => {
+    questsInput.value = guests.adults + guests.children + guests.babies;
+    questsBlock.style.visibility = "hidden";
+});
+
+guestsCancel.addEventListener( 'click', () => {
+    questsBlock.style.visibility = "hidden";
+});
+
+questsInput.addEventListener( 'click', () => {
+    questsBlock.style.visibility = "visible";
+});
